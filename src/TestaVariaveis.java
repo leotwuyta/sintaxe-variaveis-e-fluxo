1254dd18d69a1196af94cfeb43ad185507ac839e
Leonardo Reis <leonardoreissousa95@gmail.com>
2019-02-11 11:53:59 -0200

    Subindo projeto para o git hub

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,15 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+		idade = 37+5;
+		System.out.println(idade);
+		idade = 45/5;
+		System.out.println(idade);
+		idade = 90+5;
+		System.out.println("Ola meu amor "+ idade);
+	}
+}
